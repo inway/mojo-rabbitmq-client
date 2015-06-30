4c2114cca20291f36ee742ab145c870a3daaa4fe
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2015-06-30 18:03:44 +0200

    Add handler for channel close event to example, reorganize events order

--- examples/simple-test.pl
+++ examples/simple-test.pl
@@ -1,16 +1,14 @@
 #!/usr/bin/perl
 use Mojo::Base -strict;
 use Mojo::Log;
-use Mojo::IOLoop;
 use Mojo::RabbitMQ::Client;
 
-my $loop = Mojo::IOLoop->singleton;
-my $log  = Mojo::Log->new(threshold => 'trace');
+my $log = Mojo::Log->new(threshold => 'trace');
 my $amqp = Mojo::RabbitMQ::Client->new(
   url => 'rabbitmq://guest:guest@127.0.0.1:5672/');
 
-$amqp->catch(sub { $log->error("Error connecting to RabbitMQ"); });
-$amqp->on(connect => sub { $log->debug("Connected to RabbitMQ host"); });
+$amqp->catch(sub { $log->error("Error connecting to RabbitMQ") });
+$amqp->on(connect => sub { $log->debug("Connected to RabbitMQ host") });
 $amqp->on(
   open => sub {
     my ($self) = @_;
@@ -18,6 +16,7 @@ $amqp->on(
     $log->debug("Openned connection to RabbitMQ host");
 
     my $channel = Mojo::RabbitMQ::Channel->new();
+    $channel->catch(sub { $log->debug("Failed to open channel") });
     $channel->on(
       open => sub {
         my ($channel) = @_;
@@ -33,26 +32,25 @@ $amqp->on(
           immediate   => 0,
           header      => {}
         );
-        $publish->catch(sub { $log->debug("Message publish failure"); });
-        $publish->on(success => sub { $log->debug("Message published"); });
-        $publish->on(return => sub { $log->debug("Message returned to us"); });
+        $publish->catch(sub { $log->error("Message publish failure") });
+        $publish->on(success => sub { $log->debug("Message published") });
+        $publish->on(return  => sub { $log->warn("Message returned to us") });
         $publish->deliver();
 
-        my $consumer = $channel->consume(queue => 'test_queue',);
+        my $consumer = $channel->consume(queue => 'test_queue');
+        $consumer->catch(sub { $log->error("Error while consuming queue") });
         $consumer->on(success => sub { $log->debug("Consumed") });
         $consumer->on(message => sub { $log->debug("Received a message") });
-        $consumer->catch(sub { $log->debug("Error while consuming queue") });
         $consumer->deliver;
       }
     );
-    $channel->catch(
-      sub { $log->debug("Failed to open channel to RabbitMQ host"); });
+    $channel->on(close => sub { $log->error('Channel closed') });
 
     $self->open_channel($channel);
   }
 );
-$amqp->on(close      => sub { $log->error("RabbitMQ connection closed"); });
-$amqp->on(disconnect => sub { $log->error("RabbitMQ disconnected"); });
+$amqp->on(close      => sub { $log->error("RabbitMQ connection closed") });
+$amqp->on(disconnect => sub { $log->error("RabbitMQ disconnected") });
 $amqp->connect();
 
-$loop->start unless $loop->is_running;
+$amqp->start();
