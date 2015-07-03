ff2500c13faae526f0d91086698476494bf1fd9b
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2015-07-03 16:49:03 +0200

    Finish channel docs

--- lib/Mojo/RabbitMQ/Channel.pm
+++ lib/Mojo/RabbitMQ/Channel.pm
@@ -1098,9 +1098,43 @@ discarded or dead-lettered.
 
 =head2 select_tx
 
+Work with transactions.
+
+The Tx class allows publish and ack operations to be batched into atomic units of work.
+The intention is that all publish and ack requests issued within a transaction will
+complete successfully or none of them will. Servers SHOULD implement atomic transactions
+at least where all publish or ack requests affect a single queue. Transactions that cover
+multiple queues may be non-atomic, given that queues can be created and destroyed
+asynchronously, and such events do not form part of any transaction.
+Further, the behaviour of transactions with respect to the immediate and mandatory flags
+on Basic.Publish methods is not defined. 
+
+  $channel->select_tx()->deliver;
+
+Select standard transaction mode.
+
+This method sets the channel to use standard transactions. The client must use this method
+at least once on a channel before using the Commit or Rollback methods.
+
 =head2 commit_tx
 
+  $channel->commit_tx()->deliver;
+
+Commit the current transaction.
+
+This method commits all message publications and acknowledgments performed in the current
+transaction. A new transaction starts immediately after a commit. 
+
 =head2 rollback_tx
+
+  $channel->rollback_tx()->deliver;
+
+Abandon the current transaction.
+
+This method abandons all message publications and acknowledgments performed in the current
+transaction. A new transaction starts immediately after a rollback. Note that unacked messages
+will not be automatically redelivered by rollback; if that is required an explicit recover
+call should be issued. 
 
 =head1 SEE ALSO
 
