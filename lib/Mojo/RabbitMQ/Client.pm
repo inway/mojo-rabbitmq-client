260b6ec5d7ce46f225d59b9be1582040880bd417
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2016-11-24 01:45:03 +0100

    Checking in changes prior to tagging of version 0.0.6.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 40a1a9f..11d3fd9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,7 @@
    -
     {{$NEXT}}
    
    +0.0.6 2016-11-24T00:45:01Z
    +  - First CPAN release
     0.0.5  2016-11-23
       - First Minilla release
       - Drop support for Module::Install

--- lib/Mojo/RabbitMQ/Client.pm
+++ lib/Mojo/RabbitMQ/Client.pm
@@ -16,7 +16,7 @@ use Mojo::RabbitMQ::Client::Consumer;
 use Mojo::RabbitMQ::Client::LocalQueue;
 use Mojo::RabbitMQ::Client::Publisher;
 
-our $VERSION = "0.0.5";
+our $VERSION = "0.0.6";
 
 use constant DEBUG => $ENV{MOJO_RABBITMQ_DEBUG} // 0;
 
